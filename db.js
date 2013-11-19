31bf18722faf3f7f69f1d003c7358070253b568e
tlangenberg <thomas.langenberg@gmail.com>
2013-11-19 01:23:09 +0100

    Update db.js

--- db.js
+++ db.js
@@ -9,4 +9,4 @@ var Todo = new Schema({
 
 mongoose.model( 'Todo', Todo );
 
-mongoose.connect( 'mongodb://localhost/express-todo' );
+mongoose.connect( 'mongodb://ALTzpabjzDmN:ZZmSAuConmKJ@dbs001.mongosoup.de/cc_ALTzpabjzDmN' );
